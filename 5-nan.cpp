5f698317f1eb3db34ef2d31b926c3f1b6698bdbc
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-11-05 11:40:11 +0100

    Create 5-nan.cpp

---
+++ 5-nan.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 8
+ * Autores: Miguel Ángel Latre
+ * Última revisión: 23 de octubre de 2018
+ * Resumen: Programa que muestra un resultado con una codificación no válidas de
+ *          datos de tipo real.
+ ******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+ * Programa que muestra un resultado con una codificación no válidas de datos de
+ * tipo real.
+ */
+int main() {
+    double x = sqrt(-1);
+    cout << "x = " << x << endl;
+    return 0;
+}
